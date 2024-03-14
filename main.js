5ef2529076a240d66356ce636ea96750d1c1ac38
AtayevZarif <atayev1234567890@gmail.com>
2024-03-14 21:40:28 +0500

    optimizated

--- main.js
+++ main.js
@@ -2,14 +2,9 @@ let string = prompt("String typedagi malumot kiriting");
 let number = Number(prompt("Number typedagi malumot kiriting"));
 let boolean = Boolean(prompt("Boolean typedagi malumot kiriting"));
 
-var check = "Hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?";
-var words = "Siz kiritgan";
-var word = "so'zi";
-var words2 = "typega tegishli";
+let check = "Hamma malumotlarni to'g'ri kiritganizga ishonchingiz komilmi?";
 
 alert(check);
-alert(
-    words + " " + string + " " + word + " " + typeof(string) + " " + words2 + "\n" 
-    + words + " " + number + " " + word + " " + typeof(number) + " " + words2 + "\n"
-    + words + " " + boolean + " " + word + " " + typeof(boolean) + " " + words2
-);
+alert("Siz kiritgan" + " " + string + " " + "so'zi" + " " + typeof(string) + " " + "typega tegishli" + "\n"
+ + "Siz kiritgan" + " " + number + " " + "so'zi" + " " + typeof(number) + " " + "typega tegishli" + "\n"
+ + "Siz kiritgan" + " " + boolean + " " + "so'zi" + " " + typeof(boolean) + " " + "typega tegishli");
